62750f2ad6fe4f6015d7915421b78127fb37784a
Rumeysa <zasyemur@gmail.com>
2024-07-29 16:17:32 +0300

    terminalde ilk çalıştırma gerçekleşti

--- Fİyat.py
+++ Fİyat.py
@@ -1,1 +1,15 @@
 from selenium import webdriver
+import chromedriver_autoinstaller
+import time
+
+chromedriver_autoinstaller.install()
+
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.korodrogerie.de/en/nuts")
+
+time.sleep(60)
+driver.quit
+

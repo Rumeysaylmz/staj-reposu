8230cdfe05cb335332a221b3ba3d3f926e0f4f2f
Rumeysa <zasyemur@gmail.com>
2024-08-01 09:05:17 +0300

    yeni

--- Fiyat.py
+++ Fiyat.py
@@ -1,3 +1,18 @@
+from selenium import webdriver
+import chromedriver_autoinstaller
+import time
+
+chromedriver_autoinstaller.install()
+
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.korodrogerie.de/en/nuts")
+
+time.sleep(60)
+driver.quit
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
